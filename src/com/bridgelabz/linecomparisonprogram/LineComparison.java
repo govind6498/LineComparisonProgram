a2fc31dbaa33f872f5c7f272bdd6d2c2192f978e
govind6498 <govindsingh6498@gmail.com>
2021-09-14 00:48:59 +0530

    Added:To Find Line Length

--- src/com/bridgelabz/linecomparisonprogram/LineComparison.java
+++ src/com/bridgelabz/linecomparisonprogram/LineComparison.java
@@ -1,7 +1,29 @@
 package com.bridgelabz.linecomparisonprogram;
-
 public class LineComparison {
+	class Point{
+		double x,y;
+		public Point(double x,double y) {
+			this.x=x;
+			this.y=y;
+		}
+	}
+	Point p1,p2;
+	double x1,x2,y1,y2;
+	public LineComparison(double xStartPoint,double xEndPoint,double yStartPoint,double yEndPoint){
+		this.p1=new Point(xStartPoint,yStartPoint);
+		this.p2=new Point(xEndPoint,yEndPoint);
+	}
+	public double getLegth() {
+		return Math.round(Math.sqrt(Math.pow(p2.x-p1.y, 2)+Math.pow(p1.y-p2.y,2)));
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch ");
+		LineComparison L1=new LineComparison(7,20,4.8,4.4);
+		LineComparison L2=new LineComparison(1.5,1,5.3,5.4);
+
+		//use case 1
+		System.out.println("Length of Line 1="+L1.getLegth()+"\t"+"Length of Line 2="+L2.getLegth());
+		System.out.println();
 	}
 }
