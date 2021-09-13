85b903058cb30cda5f1619ba66da675ce4a0ba5c
govind6498 <govindsingh6498@gmail.com>
2021-09-14 00:54:47 +0530

    Added:To Compare two Line are Equal

--- src/com/bridgelabz/linecomparisonprogram/LineComparison.java
+++ src/com/bridgelabz/linecomparisonprogram/LineComparison.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.linecomparisonprogram;
+
+import bridgelabz.Line;
+
 public class LineComparison {
 	class Point{
 		double x,y;
@@ -17,6 +20,11 @@ public class LineComparison {
 		return Math.round(Math.sqrt(Math.pow(p2.x-p1.y, 2)+Math.pow(p1.y-p2.y,2)));
 
 	}
+	public boolean equals(LineComparison L) {
+		Double len1=this.getLegth();
+		Double len2=L.getLegth();
+		return len1.equals(len2);
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch ");
 		LineComparison L1=new LineComparison(7,20,4.8,4.4);
@@ -25,5 +33,16 @@ public class LineComparison {
 		//use case 1
 		System.out.println("Length of Line 1="+L1.getLegth()+"\t"+"Length of Line 2="+L2.getLegth());
 		System.out.println();
+
+		//Use case 2
+		boolean result1=L1.equals(L2);
+		if(result1==true) {
+			System.out.println("Length of Line 1 is Equal to Length of Line 2");
+		}
+		else {
+			System.out.println("Length of Line 1 is not equals to Line 2.");  
+		}
+
+
 	}
 }
